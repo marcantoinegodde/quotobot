a3b7bd17da750238faaaa6ec920f09eb85faf1d8
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-04-19 00:55:05 +0200

    :sparkles: Add random command

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 
 	b.RegisterHandler(bot.HandlerTypeMessageText, "add", bot.MatchTypeCommandStartOnly, qb.addHandler)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "last", bot.MatchTypeCommandStartOnly, qb.lastHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "random", bot.MatchTypeCommandStartOnly, qb.randomHandler)
 
 	b.Start(ctx)
 }
@@ -128,6 +129,61 @@ func (qb *QuotoBot) lastHandler(ctx context.Context, b *bot.Bot, update *models.
 
 	var quotes []Quote
 	if err := qb.Database.Order("id desc").Limit(n).Find(&quotes).Error; err != nil {
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   "Erreur lors de la récupération des citations",
+		})
+		log.Printf("Erreur lors de la récupération des citations: %v", err)
+		return
+	}
+
+	if len(quotes) == 0 {
+		return
+	}
+
+	separator := "\n" + strings.Repeat("_", 20) + "\n\n"
+	var formattedQuotes []string
+
+	for _, quote := range quotes {
+		formattedQuote := fmt.Sprintf("\\#Q%d _\\(\\+%d\\)_\n*%s*\n\n_by %s_",
+			quote.ID, len(quote.Votes), bot.EscapeMarkdown(quote.Content), bot.EscapeMarkdown(quote.Author))
+		formattedQuotes = append(formattedQuotes, formattedQuote)
+	}
+
+	response := strings.Join(formattedQuotes, bot.EscapeMarkdown(separator))
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    update.Message.Chat.ID,
+		Text:      response,
+		ParseMode: models.ParseModeMarkdown,
+	})
+
+	log.Printf("%d quotes envoyées à %s\n", len(quotes), update.Message.From.Username)
+}
+
+func (qb *QuotoBot) randomHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	n := 1
+
+	command := strings.Split(update.Message.Text, " ")
+	if len(command) > 1 {
+		num, err := strconv.Atoi(command[1])
+		if err != nil || num < 1 {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: update.Message.Chat.ID,
+				Text:   "Mauvais format",
+			})
+			log.Printf("Mauvais format de %s", update.Message.From.Username)
+			return
+		}
+		n = num
+	}
+
+	const maxQuotes = 10
+	if n > maxQuotes {
+		n = maxQuotes
+	}
+
+	var quotes []Quote
+	if err := qb.Database.Order("RANDOM()").Limit(n).Find(&quotes).Error; err != nil {
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "Erreur lors de la récupération des citations",
