73bd2accf4240abc8d19db189693789f241f45ee
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-04-20 20:22:23 +0200

    :sparkles: Add top command

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 	b.RegisterHandler(bot.HandlerTypeMessageText, "vote", bot.MatchTypeCommandStartOnly, qb.voteHandler)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "unvote", bot.MatchTypeCommandStartOnly, qb.unvoteHandler)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "score", bot.MatchTypeCommandStartOnly, qb.scoreHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "top", bot.MatchTypeCommandStartOnly, qb.topHandler)
 
 	b.Start(ctx)
 }
