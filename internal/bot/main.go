790ddd3dc188c8b8a6f3fa4daa4316c57bd909b4
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2025-04-28 20:55:21 +0200

    :sparkles: Add group-like syntax commands

--- internal/bot/main.go
+++ internal/bot/main.go
@@ -44,6 +44,11 @@ func (qb *QuotoBot) Start() {
 		panic(err)
 	}
 
+	botUser, err := b.GetMe(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	b.RegisterHandler(bot.HandlerTypeMessageText, "register", bot.MatchTypeCommandStartOnly, qb.registerHandler)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "add", bot.MatchTypeCommandStartOnly, qb.addHandler, qb.requireAuth)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "last", bot.MatchTypeCommandStartOnly, qb.lastHandler, qb.requireAuth)
@@ -54,6 +59,19 @@ func (qb *QuotoBot) Start() {
 	b.RegisterHandler(bot.HandlerTypeMessageText, "unvote", bot.MatchTypeCommandStartOnly, qb.unvoteHandler, qb.requireAuth)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "score", bot.MatchTypeCommandStartOnly, qb.scoreHandler, qb.requireAuth)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "top", bot.MatchTypeCommandStartOnly, qb.topHandler, qb.requireAuth)
+
+	// Register commands with group-like syntax in the absence of a better solution
+	b.RegisterHandler(bot.HandlerTypeMessageText, "register@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.registerHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "add@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.addHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "last@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.lastHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "random@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.randomHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "get@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.getHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "search@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.searchHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "vote@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.voteHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "unvote@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.unvoteHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "score@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.scoreHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "top@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.topHandler, qb.requireAuth)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "top@"+botUser.Username, bot.MatchTypeCommandStartOnly, qb.topHandler, qb.requireAuth)
 
 	qb.Logger.Info.Println("QuotoBot started")
 
